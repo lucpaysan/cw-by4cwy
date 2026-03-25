d1a328caddb09ec2a6f4435791e1f8a188b80da1
LUC <luc@192.168.10.143>
2026-03-26 00:14:44 +0800

    fix: keep wasmPaths using CDN for onnxruntime-web

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,17 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "./",
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          const info = assetInfo.name || "";
+          if (info.endsWith(".wasm")) {
+            return "assets/[name][extname]";
+          }
+          return "assets/[name]-[hash][extname]";
+        },
+      },
+    },
+  },
 });
